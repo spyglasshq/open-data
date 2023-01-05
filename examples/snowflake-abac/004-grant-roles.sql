43160639401cb397ba7f097324fa24cef526935b
Spyglass Software, Inc <121976108+spyglass-software@users.noreply.github.com>
2023-01-04 21:29:34 -0500

    Update 004-grant-roles.sql

--- examples/snowflake-abac/004-grant-roles.sql
+++ examples/snowflake-abac/004-grant-roles.sql
@@ -1,5 +1,6 @@
 GRANT USAGE ON DATABASE merchants TO ROLE payment_services_production;
 GRANT USAGE ON SCHEMA merchants.production TO ROLE payment_services_production;
+GRANT USAGE ON SCHEMA merchants.staging TO ROLE payment_services_production;
 GRANT USAGE ON SCHEMA merchants.governance TO ROLE payment_services_production;
 GRANT SELECT ON ALL TABLES IN SCHEMA merchants.production TO ROLE payment_services_production;
 GRANT SELECT ON ALL TABLES IN SCHEMA merchants.staging TO ROLE payment_services_production;
